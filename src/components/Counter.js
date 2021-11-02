773def87f26331e7d4f26864d5ca40b7e611399b
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-11-01 18:55:08 -0700

    used ternary for step 2 and 3

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,10 +46,10 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
-  /* STEP 1 */
+  const [count, setCount] = useState(1);
 
   const increment = () => {
     /* STEP 4 */
@@ -66,12 +66,16 @@ export default function Counter() {
     marginBottom: '0.3em',
     color: 'royalblue', /* STEP 2 */
   };
-
+  if (count % 2 === 0){
+    style["color"] = 'royalblue';
+  } else {
+    style["color"] = 'crimson';
+  }
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is {count % 2 == 0 ? "even" : "odd"}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
